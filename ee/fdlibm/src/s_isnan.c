f80137a4451790ca26c96704e9f60b31e3d247fb
BoGanon <michaelkey@gmail.com>
2016-11-17 18:17:21 -0500

    Move isnanf() to s_isnan.c and add underscore prefix to symbol names

--- ee/fdlibm/src/s_isnan.c
+++ ee/fdlibm/src/s_isnan.c
@@ -1,9 +1,20 @@
 #include "math_private.h"
 
-int isnan(double x)
+/* The r5900 doesn't support infinities or nan and instead uses 0x7FFFFFFF and
+   0xFFFFFFFF and sets the FE_INVALID flag when NAN exceptions occur. The only
+   case that doesn't apply is when "sqrt.s" is used on a negative number.
+   Fortunately, for most cases, the values coincide with NAN. */
+
+int __isnan(double x)
 {
-	int hx,lx;
+	int32_t hx,lx;
 	EXTRACT_WORDS(hx, lx, x);
-	return (((hx & 0x7FFFFFFF) == 0x7FF00000) && (lx != 0));
+	return (((hx & 0x7FF00000) == 0x7FF00000) && ((lx|(hx & 0x000FFFFF))));
 }
 
+int __isnanf(float x)
+{
+	int ix;
+	GET_FLOAT_WORD(ix,x);
+	return ((ix & 0x7FFFFFFF) > 0x7F800000);
+}
