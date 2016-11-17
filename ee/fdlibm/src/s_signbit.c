5aa71876883fab7e4d0f96997daa6a9fa734601e
BoGanon <michaelkey@gmail.com>
2016-11-17 18:16:30 -0500

    Change signbit(),signbitf() to __signbit(),__signbitf()

--- ee/fdlibm/src/s_signbit.c
+++ ee/fdlibm/src/s_signbit.c
@@ -1,13 +1,13 @@
 #include "math_private.h"
 
-int signbit(double d)
+int __signbit(double d)
 {
 	int32_t hx;
 	GET_HIGH_WORD(hx,d);
 	return ((hx & 0x80000000) != 0);
 }
 
-int signbitf(float f)
+int __signbitf(float f)
 {
 	int32_t ix;
 	GET_FLOAT_WORD(ix,f);
